a73285538286645e265bbd0124e0ae41d1e60dbf
Matthew Veety <mveety@gmail.com>
2024-08-25 20:53:45 -0400

    fixed string highlighting in es-mode.el

--- examples/es-mode.el
+++ examples/es-mode.el
@@ -9,16 +9,17 @@
 
 (defvar es-font-lock-keywords
   '(
-    ("#.*$" . font-lock-comment-face)
-    ("'[^\']*'" font-lock-variable-face)
+    ("#.*$" . 'font-lock-comment-face)
+    ;; ("\$*" . 'font-lock-variable-face)
     
-    ("\\<\\(let\\|if\\|for\\|while\\|fn\\)\\>" . font-lock-keyword-face)
-    ("\\<\\(access\\|break\\|catch\\|cd\\|echo\\|eval\\|exec\\|exit\\|false\\|forever\\|fork\\|if\\|limit\\|newpgrp\\|result\\|return\\|throw\\|time\\|true\\|umask\\|unwind-protect\\|var\\|vars\\|wait\\|whatis\\|while\\|%read\\)\\>" . font-lock-builtin-face)
+    ("\\<\\(let\\|if\\|for\\|while\\|fn\\)\\>" . 'font-lock-keyword-face)
+    ("\\<\\(access\\|break\\|catch\\|cd\\|echo\\|eval\\|exec\\|exit\\|false\\|forever\\|fork\\|if\\|limit\\|newpgrp\\|result\\|return\\|throw\\|time\\|true\\|umask\\|unwind-protect\\|var\\|vars\\|wait\\|whatis\\|while\\|%read\\|add\\|sub\\|mul\\|div\\|mod\\|lt\\|gt\\|eq\\)\\>" . 'font-lock-builtin-face)
     
-    ("\"[^\"]*\"" 0 font-lock-string-face t)
-    ("`{[^}]*}" 0 font-lock-variable-name-face t)
-    ("\\<-\\w*\\>" 0 font-lock-reference-face t)
-    ("\$\\w*" 0 font-lock-reference-face t)
+    ("'[^']*'" . 'font-lock-string-face)
+    ;; ("\"[^\"]*\"" . 'font-lock-string-face)
+    ("`{[^}]*}" . 'font-lock-variable-name-face)
+    ("\\<-\\w*\\>" . 'font-lock-reference-face)
+    ("\$\\w*" . 'font-lock-reference-face)
     ))
 
 (defvar es-mode-syntax-table
