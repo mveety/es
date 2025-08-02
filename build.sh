9de47257c98b62a7b734edb509bb1b6b171f85b6
Matthew Veety <mveety@gmail.com>
2025-08-02 17:06:35 -0400

    add build.sh for convenience

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PLATFORM=$(uname)
+
+# clean things up
+if test -f Makefile
+then
+	make distclean
+fi
+
+# do autoconf stuff
+./setup-autoconf.sh
+
+# run configure
+case "$PLATFORM" in
+	FreeBSD)
+		CC=clang ./configure
+		;;
+	Linux)
+		CC=gcc ./configure --prefix=/usr
+		;;
+	*)
+		echo "warning: es has not been tested on your platform"
+		# just let configure work out the best defaults
+		./configure
+esac
+
+make
+
