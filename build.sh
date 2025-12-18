6dba3db5f6fa3956dedfd388ec6b7901146c143d
Matthew Veety <mveety@gmail.com>
2025-12-18 16:32:32 -0500

    solaris/illumos support

--- build.sh
+++ build.sh
@@ -5,7 +5,14 @@ PLATFORM=$(uname)
 # clean things up
 if test -f Makefile
 then
-	make distclean
+	case "$PLATFORM" in
+		SunOS)
+			gmake distclean
+			;;
+		*)
+			make distclean
+			;;
+	esac
 fi
 
 # do autoconf stuff
@@ -15,19 +22,25 @@ fi
 case "$PLATFORM" in
 	FreeBSD)
 		CC=clang ./configure --enable-modules
+		make all
 		;;
 	Linux)
 		CC=gcc ./configure --prefix=/usr --enable-modules
+		make all
 		;;
 	Haiku)
 		./configure --prefix=/boot/home/config/non-packages --enable-modules
+		make all
+		;;
+	SunOS)
+		./configure --prefix=/usr --enable-modules
+		gmake all
 		;;
 	*)
-		echo "warning: es has not been tested on your platform"
+		echo "warning: es has not been tested on your platform and won't be automatically built"
+		echo "run 'make all' or 'gmake all' to finish if configure succeeds."
 		# just let configure work out the best defaults
 		./configure
 		;;
 esac
-
-make all
 
