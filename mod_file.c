7a28722d107961c9a06a8acc5ac649916331617a
Matthew Veety <mveety@gmail.com>
2026-02-25 03:39:16 -0800

    mod_file: add blob object to store raw binary data. add file_bread and file_bwrite

--- mod_file.c
+++ mod_file.c
@@ -113,6 +113,30 @@ file_stringify(Object *obj)
 	return estrdup(&buf[0]);
 }
 
+Object *
+newblob(size_t size)
+{
+	Object *obj;
+
+	obj = allocate_object("blob", size);
+	assert(obj);
+	obj->sysflags |= ObjectFreeWhenNoRefs;
+	obj->sysflags |= ObjectGcManaged;
+	obj->sysflags |= ObjectInitialized;
+	memset(obj->data, 0, obj->size);
+	return obj;
+}
+
+static char *
+blob_stringify(Object *obj)
+{
+	char buf[2048];
+
+	memset(&buf[0], 0, sizeof(buf));
+	snprintf(&buf[0], sizeof(buf)-1, "size = %lu", obj->size);
+	return estrdup(&buf[0]);
+}
+
 static int
 file_onload(void)
 {
@@ -120,9 +144,15 @@ file_onload(void)
 		dprintf(2, "unable to define type file\n");
 		return -1;
 	}
+	if(define_type("blob", nil, nil) < 0) {
+		dprintf(2, "unable to define type blob\n");
+		undefine_type("file");
+		return -1;
+	}
 	define_stringifier("file", &file_stringify);
 	define_onfork_callback("file", &file_onfork);
 	define_allocator("file", &file_allocate);
+	define_stringifier("blob", &blob_stringify);
 	return 0;
 }
 
@@ -256,6 +286,76 @@ PRIM(file_fd) {
 	return res;
 }
 
+PRIM(blob2string) {
+	Root r_list;
+	Object *obj = nil;
+	char *newstr = nil; Root r_newstr;
+	List *res = nil; Root r_res;
+
+	if(list == nil)
+		fail("$&blob2string", "missing argument");
+	if(list->next != nil)
+		fail("$&blob2string", "too many arguments");
+
+	gcref(&r_list, (void**)&list);
+	gcref(&r_newstr, (void**)&newstr);
+	gcref(&r_res, (void**)&res);
+
+	obj = getobject(list->term);
+	if(!obj)
+		fail("$&blob2string", "must be an object");
+	if(!object_is_type(obj, "blob"))
+		fail("$&blob2string", "must be a blob object");
+
+	newstr = gcalloc(obj->size+1, tString);
+	memset(newstr, 0, obj->size+1);
+	memcpy(newstr, obj->data, obj->size);
+
+	res = mklist(mkstr(newstr), nil);
+
+	gcrderef(&r_res);
+	gcrderef(&r_newstr);
+	gcrderef(&r_list);
+
+	return res;
+}
+
+PRIM(string2blob) {
+	Root r_list;
+	Object *obj = nil;
+	char *str = nil; Root r_str;
+	List *res = nil; Root r_res;
+	size_t slen = 0;
+
+	if(list == nil)
+		fail("$&string2blob", "missing argument");
+	if(list->next != nil)
+		fail("$&string2blob", "too many arguments");
+
+	gcref(&r_list, (void**)&list);
+	gcref(&r_str, (void**)&str);
+	gcref(&r_res, (void**)&res);
+
+	str = getstr(list->term);
+	if(!str)
+		fail("$&string2blob", "not a string");
+
+	slen = strlen(str);
+	if(slen == 0)
+		fail("$&string2blob", "empty string");
+
+	obj = newblob(slen);
+	memcpy(obj->data, str, slen);
+
+	res = mklist(mkobject(obj), nil);
+
+	gcrderef(&r_res);
+	gcrderef(&r_str);
+	gcrderef(&r_list);
+
+	return res;
+}
+
 PRIM(file_read) {
 	char *buf = nil; Root r_buf;
 	char *rstr = nil; Root r_rstr;
@@ -320,6 +420,67 @@ PRIM(file_read) {
 	return res;
 }
 
+PRIM(file_bread) {
+	char *buf = nil; Root r_buf;
+	Object *obj = nil;
+	Object *blob = nil;
+	size_t nbytes = 0;
+	ssize_t nread = 0;
+	List *res = nil; Root r_res;
+	Root r_list;
+
+	if(length(list) < 2)
+		fail("$&file_bread", "missing arguments");
+	if(length(list) > 2)
+		fail("$&file_bread", "too many arguments");
+
+	gcref(&r_list, (void **)&list);
+	gcref(&r_res, (void **)&res);
+	gcref(&r_buf, (void **)&buf);
+
+	obj = getobject(list->term);
+	if(!obj)
+		fail("$&file_bread", "$1 must be an object");
+	if(!object_is_type(obj, "file"))
+		fail("$&file_bread", "$1 must be a file object");
+
+	errno = 0;
+	nbytes = strtoll(getstr(list->next->term), nil, 10);
+	if(nbytes == 0)
+		switch(errno) {
+		case EINVAL:
+			fail("$&file_bread", "invalid input: $2 = %s", getstr(list->next->term));
+			break;
+		case ERANGE:
+			fail("$&file_bread", "conversion overflow: $2 = %s", getstr(list->next->term));
+			break;
+		}
+
+	buf = gcmalloc(nbytes);
+
+	errno = 0;
+	nread = read(file(obj)->fd, buf, nbytes);
+	if(nread < 0)
+		fail("$&file_bread", "unable to read: %s", strerror(errno));
+	if(nread == 0) {
+		res = mklist(mkstr(str("0")), nil);
+		gcrderef(&r_buf);
+		gcrderef(&r_res);
+		gcrderef(&r_list);
+		return res;
+	}
+
+	blob = newblob(nread);
+	memcpy(blob->data, buf, nread);
+	res = mklist(mkobject(blob), nil);
+	res = mklist(mkstr(str("%d", nread)), res);
+
+	gcrderef(&r_buf);
+	gcrderef(&r_res);
+	gcrderef(&r_list);
+	return res;
+}
+
 PRIM(file_write) {
 	Root r_list;
 	char *outstr = nil; Root r_outstr;
@@ -350,6 +511,41 @@ PRIM(file_write) {
 		fail("$&file_write", "unable to write: %s", strerror(errno));
 
 	gcrderef(&r_outstr);
+	gcrderef(&r_list);
+
+	return mklist(mkstr(str("%d", nwritten)), nil);
+}
+
+PRIM(file_bwrite) {
+	Root r_list;
+	ssize_t nwritten = 0;
+	Object *obj;
+	Object *blob;
+
+	if(length(list) < 2)
+		fail("$&file_bwrite", "missing arguments");
+	if(length(list) > 2)
+		fail("$&file_bwrite", "too many arguments");
+
+	gcref(&r_list, (void**)&list);
+
+	obj = getobject(list->term);
+	if(!obj)
+		fail("$&file_bwrite", "$1 must be an object");
+	if(!object_is_type(obj, "file"))
+		fail("$&file_bwrite", "$1 must be a file object");
+
+	blob = getobject(list->next->term);
+	if(!blob)
+		fail("$&file_bwrite", "$2 must be an object");
+	if(!object_is_type(blob, "blob"))
+		fail("$&file_bwrite", "$2 must be a blob object");
+
+	errno = 0;
+	nwritten = write(file(obj)->fd, blob->data, blob->size);
+	if(nwritten < 0)
+		fail("$&file_bwrite", "unable to write: %s", strerror(errno));
+
 	gcrderef(&r_list);
 
 	return mklist(mkstr(str("%d", nwritten)), nil);
@@ -409,7 +605,11 @@ MODULE(mod_file, &file_onload, &file_onunload,
 	DX(file_open),
 	DX(file_name),
 	DX(file_fd),
+	DX(blob2string),
+	DX(string2blob),
 	DX(file_read),
+	DX(file_bread),
 	DX(file_write),
+	DX(file_bwrite),
 	DX(file_seek),
 );
